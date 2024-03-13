49c568d60b512c5865c8ca3e80f2e28fd8dfb079
Ingo Schmid <is@bananenbiegerei.de>
2024-03-13 16:32:04 +0100

    Update bb-blocks and functions.php

--- functions.php
+++ functions.php
@@ -11,9 +11,6 @@ require_once get_template_directory() . '/functions/localization.php';
 // Special features of theme
 require_once get_template_directory() . '/functions/features.php';
 
-// Fill in available values in some fields
-// require_once get_template_directory() . '/functions/acf-field-values.php';
-
 // Load ACF blocks
 require_once get_template_directory() . '/functions/acf-blocks.php';
 
@@ -36,6 +33,3 @@ require_once get_template_directory() . '/functions/icons.php';
 
 // Excerpts
 require_once get_template_directory() . '/functions/excerpts.php';
-
-// Block converter and unsupported blocks
-// require_once get_template_directory() . '/functions/block-converter.php';
